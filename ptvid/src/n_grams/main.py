873bd5f1291d17362084ca739fd9387d002f28b5
Hugo Sousa <hmosousa@gmail.com>
2024-08-03 05:07:05 +0000

    Refactor the main script to be a function rather than an object

--- ptvid/src/n_grams/main.py
+++ ptvid/src/n_grams/main.py
@@ -2,60 +2,53 @@ import json
 import os
 from time import time
 
-from tqdm import tqdm
-
+from ptvid.constants import DOMAINS
 from ptvid.src.data import Data
 from ptvid.src.n_grams.results import Results
 from ptvid.src.n_grams.tester import Tester
 from ptvid.src.n_grams.trainer import Trainer
 from ptvid.src.tunning import Tunning
 from ptvid.src.utils import create_output_dir, setup_logger
-from ptvid.constants import DOMAINS
 
 
-class Run:
-    def __init__(self, dataset_name: str, sample_size: int):
-        self.current_path = os.path.dirname(os.path.abspath(__file__))
-        self.current_time = int(time())
-        self.params = self._load_params()
+def run(dataset_name: str, sample_size: int):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    current_time = int(time())
+    params = _load_params(current_path)
 
-        create_output_dir(self.current_path, self.current_time)
-        setup_logger(self.current_path, self.current_time)
+    create_output_dir(current_path, current_time)
+    setup_logger(current_path, current_time)
 
-        self.data = Data(dataset_name)
+    data = Data(dataset_name)
 
-        self.tuner = Tunning(
-            self.data,
-            DOMAINS,
-            Results,
-            Trainer,
-            Tester,
-            CURRENT_PATH=self.current_path,
-            CURRENT_TIME=self.current_time,
-            params=self.params,
-            sample_size=sample_size
-        )
+    tuner = Tunning(
+        data,
+        DOMAINS,
+        Results,
+        Trainer,
+        Tester,
+        CURRENT_PATH=current_path,
+        CURRENT_TIME=current_time,
+        params=params,
+        sample_size=sample_size,
+    )
 
-    def _load_params(self):
-        f = open(os.path.join(self.current_path, "in", "params.json"), "r", encoding="utf-8")
+    return tuner.run()
 
-        # Fail if params.json does not exist
-        if f is None:
-            raise FileNotFoundError("params.json not found")
 
+def _load_params(current_path: str):
+    params_file_path = os.path.join(current_path, "in", "params.json")
+
+    with open(params_file_path, "r", encoding="utf-8") as f:
         dict_obj = json.load(f)
 
-        if "tfidf__ngram_range" in dict_obj:
-            # Cast tfidf__ngram_range to tuple
-            for idx, elem in enumerate(dict_obj["tfidf__ngram_range"]):
-                dict_obj["tfidf__ngram_range"][idx] = tuple(elem)
+    if "tfidf__ngram_range" in dict_obj:
+        # Cast tfidf__ngram_range to tuple
+        for idx, elem in enumerate(dict_obj["tfidf__ngram_range"]):
+            dict_obj["tfidf__ngram_range"][idx] = tuple(elem)
 
-        return dict_obj
-
-    def tune(self):
-        return self.tuner.run()
+    return dict_obj
 
 
 if __name__ == "__main__":
-    runner = Run(dataset_name="liaad/PtBrVId", sample_size=3_000)
-    runner.tune()
+    run(dataset_name="liaad/PtBrVId", sample_size=3_000)
