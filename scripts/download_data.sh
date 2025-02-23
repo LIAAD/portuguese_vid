fbbdce1268f03730db7848978665dcca193237a2
Rúben Almeida <almeidaruben35@gmail.com>
2025-02-23 12:44:56 +0000

    Remove Hugo Code

--- scripts/download_data.sh
+++
@@ -1,4 +0,0 @@
-mkdir data
-curl https://drive.inesctec.pt/s/PbqBoaRSHwcigKG/download --output data/raw.zip
-unzip data/raw.zip -d data/raw
-rm data/raw.zip
--- scripts/eval/run.sh
+++
@@ -1,1 +0,0 @@
-python scripts/eval/evaluate.py
